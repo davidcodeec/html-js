822aab80eba65ad6495bb747296343ff0c831bb9
David <david.obamaja@utb.ecutbildning.se>
2023-10-11 13:27:12 +0200

    My Javascript Files Commit

---
+++ 01_Console/script.js
@@ -0,0 +1,4 @@
+/* Används för att felsöka kod */
+console.log("Hejsan")
+console.warn("varningsmeddelande")
+console.error("errormeddelande")
---
+++ 02_DataTypes/script.js
@@ -0,0 +1,23 @@
+/* 
+
+Data tyoer:
+
+String : För texter
+Number : för tal
+Boolean: Sant eller falsk
+Object: För object, attribut, egenskaper, datatyper.
+Null : Att symbolisera att någonting är inte någonting alls eller tomt
+Undefined: Att någonting är inte definerad alls. Inte ens som null.
+Any: Vad som helst.
+
+*/
+
+
+
+/* 
+Variable Deklaration änvands av camelCase:
+Which means first word starts with a lowercase while second word starts with a uppercase:
+E.g: firstName, LastName, phoneNumber.
+
+
+*/
