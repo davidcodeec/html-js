d57d5ac572132cb5e9f05d1ca691d7be8b9c55c0
David <david.obamaja@utb.ecutbildning.se>
2023-10-13 20:34:24 +0200

    My Javascript Files Commit

--- 12_DOM_och_BOM/script.js
+++ 12_DOM_och_BOM/script.js
@@ -20,6 +20,8 @@ The querySelector() method returns the first element that matches a CSS selector
 
 The querySelectorAll() method returns all elements that matches a CSS selector(s).
 
+To change the style of an HTML element, use this syntax: document.getElementById(id).style.property = new style
+
 
 */
 
@@ -80,3 +82,258 @@ element.innerText = "Test"
 let elements = document.querySelectorAll("p")
 
 elements.innerText = "Test"
+
+
+/* To change the style of an HTML element using javascript, use this syntax: */
+const elementStyling = document.querySelector("#box-four")
+
+elementStyling.style.backgroundColor = "blue"
+elementStyling.style.padding = "25px"
+
+
+/* To change the style of an HTML element using javascript querySelectorAll, use this syntax:
+This case all styling will be applied to all div with box-2.
+*/
+const elementStylingAll = document.querySelectorAll(".box-2")
+
+elementStylingAll.forEach(elementstyle =>{
+    elementstyle.style.backgroundColor = "blue"
+    elementstyle.style.padding = "15px"
+    elementstyle.style.margin = "10px"
+})
+
+/* Adding Css class into Javascript */
+const elementwithClassStyling1 = document.querySelector("#box-seven")
+const elementwithClassStyling2 = document.querySelector("#box-eight")
+
+elementwithClassStyling1.classList.add('active')
+elementwithClassStyling2.classList.add('inactive')
+
+/* Adding Css class into Javascript Function ..Same as above but using a function*/
+
+function toggleActiveState(elementName, state){
+    const elementwithClassStyling3 = document.querySelector(elementName)
+    if(state){
+        elementwithClassStyling3.classList.add('active')
+        elementwithClassStyling3.classList.remove('inactive')
+    }
+         
+    else {
+         elementwithClassStyling3.classList.add('inactive')
+         elementwithClassStyling3.classList.remove('active')   
+    } 
+}
+
+toggleActiveState('#box-nine',false)
+toggleActiveState('#box-ten',true)
+
+
+/* Adding Css class into Javascript Function and state ..Same as above but using a function*/
+
+function changeState(elementName, state){
+    const elementwithClassStyling4 = document.querySelector(elementName)
+    const status = elementwithClassStyling4.querySelector(".status")
+
+    if(state){
+        elementwithClassStyling4.classList.add('active')
+        elementwithClassStyling4.classList.remove('inactive')
+        status.innerText = "Aktiv"
+    }
+         
+    else {
+         elementwithClassStyling4.classList.add('inactive')
+         elementwithClassStyling4.classList.remove('active') 
+         status.innerText = "InAktiv"  
+    } 
+}
+
+changeState('#box-eleven',true)
+changeState('#box-twelve',false)
+
+
+/*  Or Same as above but using class name*/
+
+function changeState(elementName, state){
+    const elementwithClassStyling4 = document.querySelector(elementName)
+    const name = document.querySelector('.name')
+    const status = elementwithClassStyling4.querySelector(".status")
+
+    if(state){
+        elementwithClassStyling4.classList.add('active')
+        elementwithClassStyling4.classList.remove('inactive')
+        status.innerText = `${name.innerText} is Aktiv`
+    }
+         
+    else {
+         elementwithClassStyling4.classList.add('inactive')
+         elementwithClassStyling4.classList.remove('active') 
+         status.innerText = `${name.innerText} is InAktiv` 
+    } 
+}
+
+changeState('#box-eleven',true)
+changeState('#box-twelve',false)
+
+
+
+/* Using objects in Javascript to Print data on DOM or Screen*/
+
+const section = document.querySelector('section')
+
+const users = [
+    {
+        firstName :  "David",
+        lastName :  "John",
+        isActive :  false
+
+    },
+
+    {
+        firstName :  "Tommy",
+        lastName :  "John",
+        isActive :  true
+
+    },
+
+    {
+        firstName :  "Joakim",
+        lastName :  "John",
+        isActive :  true
+
+    }
+
+]
+
+
+/* Using function with innerText to print to DOm working*/
+/* for (let user of users){
+    console.log(`${user.firstName} ${user.lastName}`)
+    section.innerText += `${user.firstName} ${user.lastName}`
+} */
+
+
+/* Using function with innerHTML to print to DOm working*/
+/* for (let user1 of users){
+
+    section.innerHTML += 
+
+    `<div>
+        <p>${user1.firstName} ${user1.lastName}</p>
+        <p>${user1.isActive}</p>
+    </div>
+    `
+}
+ */
+
+
+
+/* Using function with if statment and innerHTML to print to DOm
+You can also adjust the css for the div.
+*/
+/* for (let user of users){
+
+    if (user.isActive){
+        section.innerHTML += 
+
+    `<div class="active">
+        <p>${user.firstName} ${user.lastName}</p>
+        <p>${user.isActive}</p>
+    </div>
+    `
+    }else {
+
+        section.innerHTML += 
+
+    `<div class="inactive">
+        <p>${user.firstName} ${user.lastName}</p>
+        <p>${user.isActive}</p>
+    </div>
+    `
+    }
+
+}  
+ */
+
+ /* Another way to write if for the code above */
+
+/*  for (let user of users){
+
+    section.innerHTML += 
+
+ `<div class="${user.isActive ? "active" : "inactive"}">
+     <p>${user.firstName} ${user.lastName}</p>
+     <p>${user.isActive}</p>
+     
+ </div>
+ `
+
+}   */
+
+
+/* Using Javascript Event to collect data from users declared above */
+ const button = document.querySelector("#addBtn")
+
+ addBtn.addEventListener("click", (event) => {
+
+    /* This emptys the page after every click instead of repeating the code */
+    section.innerHTML = ""
+
+
+    for (let user of users){
+
+        section.innerHTML += 
+    
+     `<div class="${user.isActive ? "active" : "inactive"}">
+         <p>${user.firstName} ${user.lastName}</p>
+         <p>${user.isActive}</p>
+         
+     </div>
+     `
+
+    }    
+
+ })
+
+
+ const clearBtn = document.querySelector("#clearBtn")
+ clearBtn.addEventListener("click", (event) => {
+    
+     /* This emptys the page after every click instead of repeating the code */
+     section.innerHTML = ""
+
+ })
+
+
+
+
+ /* Using the onclick with a function two ways of writing..Same as above but using onclick*/
+
+ const clearUsers = () => {
+    section.innerHTML = ""
+ }
+
+
+ function showList(){
+
+    /* Same as  section.innerHTML = "" */
+    clearUsers()
+
+    for (let user of users){
+
+        section.innerHTML += 
+    
+     `<div class="${user.isActive ? "active" : "inactive"}">
+         <p>${user.firstName} ${user.lastName}</p>
+         <p>${user.isActive}</p>
+         
+     </div>
+     `
+
+    }    
+ }
+ 
+ 
+ 
+
+
+
