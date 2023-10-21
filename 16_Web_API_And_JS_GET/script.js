3c7ad0b7510b6a71a9f1b02346ad9317dd452290
David <david.obamaja@utb.ecutbildning.se>
2023-10-21 11:57:46 +0200

    My Javascript Files Commit

--- 16_Web_API_And_JS_GET/script.js
+++ 16_Web_API_And_JS_GET/script.js
@@ -48,19 +48,8 @@ const getUsers = async () => {
     const users = await result.json()
     console.log(users)
 
-    for(let user of users){
+    return users
 
-        document.getElementById('result').innerHTML +=
-    
-        `
-        <a href="profile.html?id=${user.id}" class="list-group-item list-group-item-action py-3">
-            <div><strong>${user.firstName}  ${user.lastName} </strong> </div>
-            <div><small> ${user.email} </small> </div>
-        </a>
-        
-        `
-
-    }
 }
 
   catch(error){
@@ -72,47 +61,17 @@ const getUsers = async () => {
 
 
 // Another way of writing the async function using the arrow function
-const getUser = async () => {
+const getUser = async (id) => {
 
-    const params = new URLSearchParams(window.location.search)
-      const id = params.get('id')
-      console.log(id)
-
+  
 
     try {
       
         const result = await fetch(`http://localhost:5500/api/users/user?id=${id}`)
         const user = await result.json()
         console.log(user)
-    
-  
-          document.getElementById('result').innerHTML =
-      
-          `
 
-        <div>
-            <div class= 'mb-3' >
-                 <div>><small> User ID </small> </div>
-                 <div> >${user.id} </div>
-            </div>
-            
-            <div>
-                 <div>><small> First Name </small> </div>
-                 <div> >${user.firstName} </div>
-            </div>
-
-            <div>
-                <div>><small> Email </small> </div>
-                <div> >${user.email} </div>
-            </div>
-
-            <div>
-                <div>><small> Phone </small> </div>
-                <div> >${user.phone} </div>
-            </div>
-        <div>
-          
-          `
+        return user
   
   }
   
@@ -124,3 +83,277 @@ const getUser = async () => {
 
 
 
+  const displayUsers = async () => {
+
+    const users = await getUsers()
+
+    
+     for(let user of users){
+
+        document.getElementById('result').innerHTML +=
+    
+        `
+        <a href="profile.html?id=${user.id}" class="list-group-item list-group-item-action py-3">
+            <div><strong>${user.firstName}  ${user.lastName} </strong> </div>
+            <div><small> ${user.email} </small> </div>
+        </a>
+        
+        `
+
+    } 
+
+  }
+
+
+  const displayUser = async () => {
+
+    const params = new URLSearchParams(window.location.search)
+    const id = params.get('id')
+    console.log(id)
+
+
+    const user = await getUser(id)
+
+    
+     document.getElementById('result').innerHTML =
+      
+          `
+
+        <div>
+            <div class= 'mb-3' >
+                 <div><small> User ID </small> </div>
+                 <div>${user.id} </div>
+            </div>
+            
+            <div class= 'mb-3'>
+                 <div><small> First Name </small> </div>
+                 <div>${user.firstName} ${user.lastName} </div>
+            </div>
+
+            <div class= 'mb-3'>
+                <div><small> Email </small> </div>
+                <div>${user.email} </div>
+            </div>
+
+            <div class= 'mb-5'>
+                <div><small> Phone </small> </div>
+                <div>${user.phone} </div>
+            </div>
+
+            <div class= 'mb-5'>
+                <button class='btn btn-danger me-2'>DELETE</button>
+                <a href="edit.html?id=${id}" class="btn btn-warning">EDIT</a>
+            </div>
+
+        <div>
+          
+          ` 
+
+  }
+
+
+
+  const displayEditUser = async () => {
+
+    const params = new URLSearchParams(window.location.search)
+    const id = params.get('id')
+    console.log(id)
+
+
+    const user = await getUser(id)
+
+     // You can also use for loop to get the values too...
+     document.getElementById('firstName').value = user.firstName
+     document.getElementById('lastName').value = user.lastName
+     document.getElementById('email').value = user.email
+     document.getElementById('phone').value = user.phone
+
+  }
+
+
+
+  const handleEdit = async (event) => {
+
+    event.preventDefault()
+
+    let errors = []
+
+
+    for(let i=0; i<event.target.length; i++){
+
+        if(event.target[i].required){
+           let isValid = validate(event.target[i])  
+           errors.push(isValid)
+
+        } 
+    }
+
+
+    if(!errors.includes(false)){
+        console.log('Nu är formularet validerat och klart och allt är OK')
+
+            const json = JSON.stringify({
+                firstName: event.target['firstName'].value,
+                lastName: event.target['lastName'].value,
+                email : event.target['email'].value,
+                phone: event.target['phone'].value,
+                }) 
+
+            
+                const res = await fetch('http://localhost:5500/api/users', {
+                    method : 'post',
+                    // mode:"no-cors", 
+                    // This tells that the content type is a json format 
+                    headers : {
+                        "Content-Type": "application/json",
+                       // "Access-Control-Allow-Origin": "*",
+                       // "Access-Control-Allow-Credentials" : true 
+                    },
+                    // This is the content which is the json object we converted above 
+                    body :  json,
+                  }) // Then is used after fetch method if everything is okey.
+                    
+                    // Get a response or result back and stored as data if everything is ok
+        
+                    let data
+                    if(res.status === 201 ){
+                        // Used to show an alert of success on the html if the code works    
+                        document.getElementById('status-messages').innerHTML = `<div class="alert alert-success" role="alert"> User was created successfully! </div>`
+                        data = await res.json()
+                  }else {
+                        data = res.text()
+                        // Used to show an alert on the html if the code doesnt work  
+                    document.getElementById('status-messages').innerHTML = `<div class="alert alert-alert" role="alert"> ${data}! </div>`
+                    }    
+
+            
+        }
+  }
+
+
+
+
+
+  // FOr the Validation of the Username email and Password
+
+  /* Access the element using target using validateFormField target instead of if statement as seen above*/
+function validate(element){
+
+    // You can create errorMessages variable for the errors or get it directly from the event but getting it 
+    // fromt he event and using it is much difficult...
+    const errorMessages = {
+        firstName_required : 'You must enter a first name',
+        firstName_invalid : 'You must enter a valid first name',
+        lastName_required : 'You must enter a last name',
+        lastName_invalid : 'You must enter a valid last name',
+        email_required : 'You must enter a e-postaddress',
+        email_invalid : 'You must enter a valid e-postaddress',
+        password_required : 'You must enter a password',
+        password_invalid : 'You must enter a valid and a strong password',
+        comfirmPassword_required : 'You must comfirm password',
+        comfirmPassword_invalid : 'Password doesnt match'
+    }
+
+    // If the lenght of the value is 1 or less than 1
+    if(!validateLength(element.value, 1)) {
+        document.getElementById(`${element.id}`).classList.add('error')
+        document.getElementById(`${element.id}-error`).innerHTML= errorMessages[element.id + '_required']
+        return false
+    }else{
+
+        let result = false
+
+        /* Used to valid the email using regex along with the validFormField...code same as below but much better*/
+        switch(element.type){
+            case 'text' :
+                result = validateName(element.value)  
+                break;
+            /* Validation for both email and password not working...??? */    
+            case 'email' :
+                result = validateEmail(element.value)
+                break;
+            case 'password' :
+                result = validatePassword(element)
+                break;    
+
+        }
+
+
+        if(!result){
+            // If the valuse is not correct or is invalid or result is false
+            document.getElementById(`${element.id}`).classList.add('error')
+            document.getElementById(`${element.id}-error`).innerHTML= errorMessages[element.id + '_invalid']
+            return false
+        } else {
+            // If result is true and the value is valid or written correctly
+            document.getElementById(`${element.id}`).classList.remove('error')
+            document.getElementById(`${element.id}-error`).innerHTML=''
+            return true
+        }
+
+    }
+
+
+} 
+
+
+
+
+  const validateName = (value) => {
+
+    if (/^[A-Z][a-zA-Z'-]{1,}$/.test(value) && validateLength(value)) {
+       
+        return true
+ }     
+    return false
+
+    
+}
+
+
+
+function validateLength(value,minLength=2) {
+
+    if (value !== "" && value.length >= minLength) 
+            return true
+
+        return false
+
+    
+} 
+
+function validateEmail(value) {
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+       
+            return true
+     }     
+        return false
+
+    
+} 
+
+function validatePassword(element) {
+
+    if(element.getAttribute('data-comparewith') !== null )
+        return compareValues(element)
+
+    if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(element.value)) {
+           
+            return true
+        }
+
+        return false
+
+    
+} 
+
+
+function compareValues(element){
+    let compareElement = document.getElementById(`${element.getAttribute('data-comparewith')}`)
+    
+    if(element.value === compareElement.value)
+        return true
+
+    return false
+}
